cc4cfc4deff59c3e42184738c32c0efca25444ac
EnixWord <116887494+EnixWord@users.noreply.github.com>
2023-01-05 16:16:45 +0300

    Add images to popup window

--- index.js
+++ index.js
@@ -1,18 +1,17 @@
 openNav = () => {
-  document.getElementById(
-    'myNav',
-  ).style.width = '100%';
+  document.getElementById("myNav").style.width = "100%";
 };
+
 closeNav = () => {
-  document.getElementById(
-    'myNav',
-  ).style.width = '0%';
+  document.getElementById("myNav").style.width = "0%";
 };
-const buttonPopup = document.querySelector('button');
-const modalPopup = document.querySelector('.block-popup');
-function Display(){
-  modalPopup.style.display = 'block';
+
+const buttonPopup = document.querySelector("button");
+const modalPopup = document.querySelector(".block-popup");
+function Display() {
+  modalPopup.style.display = "block";
 }
-function closePopup(){
-  modalPopup.style.display = 'none';
+
+function closePopup() {
+  modalPopup.style.display = "none";
 }
