6273ac13b0bd577450e39affbf672da884917ac7
EnixWord <116887494+EnixWord@users.noreply.github.com>
2023-01-08 00:53:56 +0300

    Complete popup-menu to match figma design

--- index.js
+++ index.js
@@ -1,31 +1,176 @@
-/* eslint-disable no-unused-vars */
-const openNav = () => {
-  document.getElementById('myNav').style.width = '100%';
-};
+// const openNav = () => {
+//   document.getElementById('myNav').style.width = '100%';
+// };
 
-const closeNav = () => {
-  document.getElementById('myNav').style.width = '0%';
-};
-const project = {
-  name: 'portfolio',
-  description:
+// const closeNav = () => {
+//   document.getElementById('myNav').style.width = '0%';
+// };
+const projects = [
+  {
+    name: 'Tonic',
+    des: ['Canopy', 'backend', '2015'],
+    img: './images/Snapshoot Portfolio.svg',
+    description:
     'Personal portfolio that contains project information, skills, and contact',
-  technologies: ['HTML', 'CSS', 'JavaScript'],
-  liveVersion: 'https://enixword.github.io/My_Portfolio/ ',
-  sourceCode: 'https://github.com/EnixWord/My_Portfolio',
-};
+    desc1: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took galley of type and scrambled it 1960s with the releorem Ipsum is simply dummy text of the printing and typesetting industry.',
+    technologies: ['HTML', 'CSS', 'JavaScript'],
+    technologies1: ['HTML', 'CSS', 'JavaScript'],
+    liveVersion: 'https://enixword.github.io/My_Portfolio/ ',
+    sourceCode: 'https://github.com/EnixWord/My_Portfolio',
+  },
+  {
+    name: 'Multi-Post Stories',
+    des: ['Canopy', 'backend', '2015'],
+    img: './images/Snapshoot Portfolio (1).svg',
+    description: 'Personal portfolio that contains project information, skills, and contact',
+    desc1: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took galley of type and scrambled it 1960s with the releorem Ipsum is simply dummy text of the printing and typesetting industry.',
+    technologies: ['HTML', 'CSS', 'JavaScript'],
+    technologies1: ['HTML', 'CSS', 'JavaScript'],
+    liveVersion: 'https://enixword.github.io/My_Portfolio/ ',
+    sourceCode: 'https://github.com/EnixWord/My_Portfolio',
+  },
+  {
+    name: 'Facebook 360',
+    des: ['Canopy', 'backend', '2015'],
+    img: './images/Snapshoot Portfolio (2).svg',
+    description:
+    'Personal portfolio that contains project information, skills, and contact',
+    desc1: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took galley of type and scrambled it 1960s with the releorem Ipsum is simply dummy text of the printing and typesetting industry.',
+    technologies: ['HTML', 'CSS', 'JavaScript'],
+    technologies1: ['HTML', 'CSS', 'JavaScript'],
+    liveVersion: 'https://enixword.github.io/My_Portfolio/ ',
+    sourceCode: 'https://github.com/EnixWord/My_Portfolio',
+  },
+  {
+    name: 'Uber Navigation',
+    des: ['Canopy', 'backend', '2015'],
+    img: './images/Snapshoot Portfolio (3).svg',
+    description:
+    'Personal portfolio that contains project information, skills, and contact',
+    desc1: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took galley of type and scrambled it 1960s with the releorem Ipsum is simply dummy text of the printing and typesetting industry.',
+    technologies: ['HTML', 'CSS', 'JavaScript'],
+    technologies1: ['HTML', 'CSS', 'JavaScript'],
+    liveVersion: 'https://enixword.github.io/My_Portfolio/ ',
+    sourceCode: 'https://github.com/EnixWord/My_Portfolio',
+  },
+];
 
-const buttonPopup = document.querySelector('button');
-const modalPopup = document.querySelector('.block-popup');
-const closeButton = document.querySelector('#close-btn');
-function Display() {
-  modalPopup.style.display = 'block';
+const workpage = document.querySelector('#workpage');
+for (let i = 0; i < projects.length; i += 1) {
+  workpage.innerHTML += `
+  <div class="project-card flex column">
+  <img src="${projects[i].img}" alt="project card image" />
+  <div class="prject-card-info flex column">
+    <h2 class="project-title">${projects[i].name}</h2>
+    <div class="project-meta flex">
+      <p class="bold">${projects[i].des[0]}</p>
+      <div class="round"></div>
+      <p>${projects[i].des[1]}</p>
+      <div class="round"></div>
+      <p>${projects[i].des[2]}</p>
+    </div>
+    <div class="project-note">
+      <p>
+      ${projects[i].description}
+      </p>
+    </div>
+    <div class="project-tag">
+      <ul class="project-skills flex">
+        <li>${projects[i].technologies[0]}</li>
+        <li>${projects[i].technologies[1]}</li>
+        <li>${projects[i].technologies[2]}</li>
+      </ul>
+    </div>
+    <button type="button" id="btn-${i}">See Project</button>
+  </div>
+  `;
 }
-function closePopup() {
-  modalPopup.style.display = 'none';
+
+const modal = document.getElementById('modal1');
+
+for (let j = 0; j < projects.length; j += 1) {
+  const seeBtn = document.querySelectorAll(`[id=btn-${j}]`);
+  seeBtn.forEach((bt) => {
+    bt.addEventListener('click', () => {
+      modal.style.display = 'block';
+      modal.innerHTML = `
+      <div class="popup-container block-popup">
+      <div id="popup-modal" class="pop-content">
+        <div class="headline-popup">
+          <h1>${projects[j].name}</h1>
+          <a href="#" role="button" onclick="closePopup()">
+            <div class="popup-clsbtn">x</div>
+          </a>
+        </div>
+      </div>
+      <ul class="small-tags">
+        <li class="canopy-sm-tag">${projects[j].des[0]}</li>
+        <li>
+          <div class="round-popup"></div>
+        </li>
+        <li class="second-sm-tag">${projects[j].des[1]}</li>
+        <li>
+          <div class="round-popup"></div>
+        </li>
+        <li class="second-sm-tag">${projects[j].des[2]}</li>
+      </ul>
+      <div class="popup-image-container">
+        <img src="${projects[j].img}" alt="pop-image" />
+      </div>
+      <div class="buttom-content-container">
+        <div class="paragraph-description">
+          <p class="paragraph-description-desktop">
+            ${projects[j].desc1}
+          </p>
+        </div>
+        <div class="action-content">
+          <div class="popup-tags">
+            <ul class="first">
+            <li>${projects[j].technologies[0]}</li>
+            <li>${projects[j].technologies[1]}</li>
+            <li>${projects[j].technologies[2]}</li>
+            </ul>
+            <ul class="second">
+            <li>${projects[j].technologies1[0]}</li>
+            <li>${projects[j].technologies1[1]}</li>
+            <li>${projects[j].technologies1[2]}</li>
+            </ul>
+          </div>
+          <div class="divider-popup">
+            <img src="./images/Vector 4.png" alt="Divider popup" />
+          </div>
+          <div class="popup-button-action">
+            <button type="submit">
+              <div class="source-code">
+                <a href="">See live </a>
+              </div>
+              <div class="deploy-icon">
+                <img src="./images/deploy.svg" alt="deployment icon" class="deploy-icon-popup" />
+              </div>
+            </button>
+            <button class="submit-github">
+              <div class="live-version">
+                <a href="">See Source </a>
+              </div>
+              <div class="github-img">
+                <img src="./images/github-popup.svg" alt="github icon" class="github-icon-popup" />
+              </div>
+
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+      `;
+
+      const closeBtn = document.querySelector('.popup-clsbtn');
+      closeBtn.addEventListener('click', () => {
+        const modal = document.querySelector('.modal');
+        modal.style.display = 'none';
+      });
+    });
+  });
 }
-buttonPopup.addEventListener('click', Display);
-closeButton.addEventListener('click', closePopup);
 
 // ---------------------------Contact form validation---------------------
 const formContact = document.getElementById('form');
@@ -104,18 +249,3 @@ window.onload = function objectStorage() {
   // Store data when user changes an existing data
   fullName.addEventListener('input', saveToLocalStorage);
 };
-const Background = document.querySelector('.works');
-function Display() {
-  modalPopup.style.display = 'block';
-  Background.style.opacity = '0.8';
-  Background.style.backgroundColor = '#C1C7D0';
-  Background.style.backdropFilter = 'blur(10px)';
-  Background.style.mixBlendMode = 'multiply';
-}
-function closePopup() {
-  modalPopup.style.display = 'none';
-  Background.style.opacity = '1';
-  Background.style.backgroundColor = '#E5E5E5';
-  Background.style.backdropFilter = 'none';
-  Background.style.mixBlendMode = 'normal';
-}
